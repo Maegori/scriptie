94754756edb6827d1736323c3ed1406b89fbb884
Maegori <lex.j.12081@gmail.com>
2021-06-04 02:02:30 +0200

    sync

--- preprocess.py
+++ preprocess.py
@@ -1,142 +1,191 @@
-import plotly.express as px
-import essentia
-import essentia.standard  as es
-import numpy as np 
-from midiutil.MidiFile import MIDIFile
 import os
 
+import essentia
+
+essentia.log.warningActive = False
+essentia.log.infoActive = False
+
+import essentia.standard as es
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+# from essentia.standard import *
+from midiutil.MidiFile import MIDIFile
 from scipy.signal import medfilt
 
-def save_midi(output_fn, notes, tempo):
 
-    track = 0
-    time = 0
-    midifile = MIDIFile(1)
+class Preprocessor():
+    def __init__(self):
+        #initiate algorithms
+        self.pitch_extractor = es.PredominantPitchMelodia(frameSize=1024, hopSize=128, guessUnvoiced=True)
+        self.rythm_extractor = es.RhythmExtractor2013()
 
-    midifile.addTrackName(track, time, "MIDI TRACK")
-    midifile.addTempo(track, time, tempo)
+        self.wind = es.Windowing(type='hann')
+        self.spec = es.Spectrum()
+        self.mfcc = es.MFCC()
+        self.sbic = es.SBic(minLength=3)
 
-    channel = 0
-    volume = 100
+    def save_midi(self, output_fn, notes, tempo):
 
-    for note in notes:
-        onset = note[0] * (tempo/60.)
-        duration = note[1] * (tempo/60.)
-        # duration = 1
-        pitch = int(note[2])
-        # print(pitch)
-        midifile.addNote(track, channel, pitch, onset, duration, volume)
+        track = 0
+        time = 0
+        midifile = MIDIFile(1)
 
-    with open(output_fn, "wb") as f:
-        midifile.writeFile(f)
+        midifile.addTrackName(track, time, "MIDI TRACK")
+        midifile.addTempo(track, time, tempo)
 
-def midi_to_notes(midi, fs, hop, smooth, minduration):
+        channel = 0
+        volume = 100
 
-    # smooth midi pitch sequence first
-    if (smooth > 0):
-        filter_duration = smooth  # in seconds
-        filter_size = int(filter_duration * fs / float(hop))
-        if filter_size % 2 == 0:
-            filter_size += 1
-        midi_filt = medfilt(midi, filter_size)
-    else:
-        midi_filt = midi
-    # print(len(midi),len(midi_filt))
+        for note in notes:
+            onset = note[0] * (tempo/60.)
+            duration = note[1] * (tempo/60.)
+            # duration = 1
+            pitch = int(note[2])
+            # print(pitch)
+            midifile.addNote(track, channel, pitch, onset, duration, volume)
 
-    notes = []
-    p_prev = 0
-    duration = 0
-    onset = 0
-    for n, p in enumerate(midi_filt):
-        if p == p_prev:
-            duration += 1
+        with open(output_fn, "wb") as f:
+            midifile.writeFile(f)
+
+    def midi_to_notes(self, midi, fs, hop, smooth, minduration):
+
+        # smooth midi pitch sequence first
+        if (smooth > 0):
+            filter_duration = smooth  # in seconds
+            filter_size = int(filter_duration * fs / float(hop))
+            if filter_size % 2 == 0:
+                filter_size += 1
+            midi_filt = medfilt(midi, filter_size)
         else:
-            # treat 0 as silence
-            if p_prev > 0:
-                # add note
-                duration_sec = duration * hop / float(fs)
-                # only add notes that are long enough
-                if duration_sec >= minduration:
-                    onset_sec = onset * hop / float(fs)
-                    notes.append((onset_sec, duration_sec, p_prev))
+            midi_filt = midi
+        # print(len(midi),len(midi_filt))
 
-            # start new note
-            onset = n
-            duration = 1
-            p_prev = p
+        notes = []
+        p_prev = 0
+        duration = 0
+        onset = 0
+        for n, p in enumerate(midi_filt):
+            if p == p_prev:
+                duration += 1
+            else:
+                # treat 0 as silence
+                if p_prev > 0:
+                    # add note
+                    duration_sec = duration * hop / float(fs)
+                    # only add notes that are long enough
+                    if duration_sec >= minduration:
+                        onset_sec = onset * hop / float(fs)
+                        notes.append((onset_sec, duration_sec, p_prev))
 
-    # add last note
-    if p_prev > 0:
-        # add note
-        duration_sec = duration * hop / float(fs)
-        onset_sec = onset * hop / float(fs)
-        notes.append((onset_sec, duration_sec, p_prev))
+                # start new note
+                onset = n
+                duration = 1
+                p_prev = p
 
-    return notes
+        # add last note
+        if p_prev > 0:
+            # add note
+            duration_sec = duration * hop / float(fs)
+            onset_sec = onset * hop / float(fs)
+            notes.append((onset_sec, duration_sec, p_prev))
 
-
-def hz2midi(hz):
-
-    # convert from Hz to midi note
-    hz_nonneg = hz.copy()
-    idx = hz_nonneg <= 0
-    hz_nonneg[idx] = 1
-    midi = 69 + 12*np.log2(hz_nonneg/440.)
-    midi[idx] = 0
-
-    # round
-    midi = np.round(midi)
-
-    return midi
+        return notes
 
 
-def audio_to_midi(input_fn, output_fn, smooth=0.25, minduration=0.1):
-    fs = 44100
-    hop = 128
+    def hz2midi(self, hz):
 
-    audio = es.EqloudLoader(filename=input_fn, sampleRate=fs)()
-    pitch_extractor = es.PredominantPitchMelodia(frameSize=2048, hopSize=128)
-    pitch_values, pitch_confidence = pitch_extractor(audio)
+        # convert from Hz to midi note
+        hz_nonneg = hz.copy()
+        idx = hz_nonneg <= 0
+        hz_nonneg[idx] = 1
+        midi = 69 + 12*np.log2(hz_nonneg/440.)
+        midi[idx] = 0
 
-    tempo = es.RhythmExtractor2013()(audio)
+        # round
+        midi = np.round(midi)
 
-    bpm = tempo[0]
+        return midi
 
-    pitch_values = np.insert(pitch_values, 0, values=[0]*8)
+    def segment(self, audio):
+        pool = essentia.Pool()
 
-    midi_pitch = hz2midi(pitch_values)
-    notes = midi_to_notes(midi_pitch, fs, hop, smooth, minduration)
+        for frame in es.FrameGenerator(audio, frameSize=1024, hopSize=512, startFromZero=True):
+            _, mfcc_coeffs = self.mfcc(self.spec(self.wind(frame)))
+            pool.add('lowlevel.mfcc', mfcc_coeffs)
 
-    save_midi(output_fn, notes, bpm)
+        segments = self.sbic(pool['lowlevel.mfcc'].T)
 
+        return 0 if len(segments) == 2 else int(segments[1])
+
+
+    def audio_to_midi(self, input_fn, output_fn, smooth=0.25, minduration=0.1):
+        fs = 44100
+        hop = 128
+
+        audio = es.EqloudLoader(filename=input_fn, sampleRate=fs)()
+        start = self.segment(audio)
+        audio = audio[start:]
+        pitch_values, _ = self.pitch_extractor(audio)
+
+        try:
+            bpm = self.rythm_extractor(audio)[0]
+        except:
+            print(f"Skipping {input_fn} because of rythm extractor")
+            return
+
+        pitch_values = np.insert(pitch_values, 0, values=[0]*8)
+
+        midi_pitch = self.hz2midi(pitch_values)
+        notes = self.midi_to_notes(midi_pitch, fs, hop, smooth, minduration)
+
+        self.save_midi(output_fn, notes, bpm)
 
 if __name__ == "__main__":
-    # path = "../data/2020/San Marino_Freaky!_Senhit.mp3"
+    # path = "data/2020/San Marino_Freaky!_Senhit.mp3"
     # output_fn = "midi_output/San Marino_Freaky!_Senhit.mid"
 
-    # input_fns = os.listdir("../data/2020")
-    # output_fns = [name.split(".")[0] + ".mid" for name in input_fns]
+    proc = Preprocessor()
+
+    for year in range(2011, 2021):
+        print(year)
+        input_fns = os.listdir("data/"+str(year))
+        output_fns = [name.split(".")[0] + ".mid" for name in input_fns]
+        try:
+            os.mkdir("midi_output/"+str(year))
+        except FileExistsError:
+            pass
+        for idx in range(len(input_fns)):
+            proc = Preprocessor()
+            proc.audio_to_midi("data/"+str(year)+"/"+input_fns[idx], "midi_output/"+str(year)+"/"+output_fns[idx])
     
     # for idx in range(len(input_fns)):
     #     print(input_fns[idx])
-    #     audio_to_midi("../data/2020/" + input_fns[idx], "midi_output/" + output_fns[idx])
+    #     audio_to_midi("data/2020/" + input_fns[idx], "midi_output/" + output_fns[idx])
 
-    sr = 44100
 
-    audio = es.EqloudLoader(filename="../data/2020/Iceland_Think About Things_Daði & Gagnamagnið.mp3", sampleRate=44100)()
+    # proc.audio_to_midi("/home/lex/Documents/scriptie/data/2020/Iceland_Think About Things_Daði & Gagnamagnið.mp3", "test.mid")
+
+    # for idx in range(len(input_fns)):
+    #     print(input_fns[idx])
+    #     audio = es.EqloudLoader(filename="data/1956/" + input_fns[idx], sampleRate=44100)()
+    #     segment(audio)
+
+    
     # audio_to_midi(path, output_fn)
 
 
-    print(len(audio) / sr)
+    # print(len(audio) / sr)
 
-    pitch_extractor = es.PredominantPitchMelodia(frameSize=2048, hopSize=128)
-    pitch_values, pitch_confidence = pitch_extractor(audio)
+    # pitch_extractor = es.PredominantPitchMelodia(frameSize=2048, hopSize=128)
+    # pitch_values, pitch_confidence = pitch_extractor(audio)
 
-    pitch_times = np.linspace(0.0, len(audio) / sr, len(pitch_values))
+    # pitch_times = np.linspace(0.0, len(audio) / sr, len(pitch_values))
 
-    # %%
-    fig = px.line(x=pitch_times, y=pitch_values)
-    fig.show()
+    # # %%
+    # fig = px.line(x=pitch_times, y=pitch_values)
+    # fig.show()
 
 # # f, axarr = plt.subplots(2, sharex=True)
 # # axarr[0].plot(pitch_times, pitch_values)
